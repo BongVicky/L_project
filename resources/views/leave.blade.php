d4dbdca07e24a093798274ceea719acad46d07d3
Thai <khemthai32@gmail.com>
2018-07-30 18:30:35 +0700

    leave page

--- resources/views/leave.blade.php
+++ resources/views/leave.blade.php
@@ -1,4 +1,102 @@
 @extends('main')
 @section('content')
-    <h1>Hello World</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <fieldset class="box">
+                <legend align="center"><h1>Annual leave application form</h1></legend>
+                <div class="jumbotron">
+                    <h4>This form is for employees to use to apply to take annual leave. For more information about this
+                        application, please contact directly to HR department</h4>
+                </div>
+                <hr>
+                <h2> Employee's detail</h2>
+                <br>
+                {!! Form::open(['url' => 'foo/bar']) !!}
+                <div class="row">
+                    <div class="col-md-4">
+                        {{ Form::label('employee_name','Employee_Name:', array('style' => 'font-size: 20px;')) }}
+                    </div>
+                    <div class="col-md-8">
+                        {{ Form::text('employee',null, array('class' => 'form-control', 'placeholder' => 'auto fill'))}}
+                    </div>
+                </div>
+                {!! Form::close() !!}
+                {!! Form::open(['url' => 'foo/bar']) !!}
+
+                <div class="row">
+                    <div class="col-md-4">
+                        {{ Form::label('position','Position:', array('style' => 'font-size: 20px;')) }}
+                    </div>
+                    <div class="col-md-8">
+                        {{ Form::text('position',null, array('class' => 'form-control', 'placeholder' => 'auto fill'))}}
+                    </div>
+                </div>
+                {!! Form::close() !!}
+                <div class="row">
+                    <div class="col-md-4">
+                        {{ Form::label('contact_number','Mobile Phone:', array('style' => 'font-size: 20px;')) }}
+                    </div>
+                    <div class="col-md-8">
+                        {{ Form::text('contact_number',null, array('class' => 'form-control', 'placeholder' => 'auto fill'))}}
+                    </div>
+                </div>
+                {!! Form::close() !!}
+                <hr>
+
+                <h2> Leave type</h2>
+                <br>
+                {!! Form::open(['url' => 'foo/bar']) !!}
+                <div class="row">
+                    <div class="col-md-1">
+                        {{ Form::label('type','Kind:', array('style' => 'font-size: 20px;')) }}
+                    </div>
+                    <div class="col-md-5">
+                        {{ Form::text('type',null, array('class' => 'form-control', 'placeholder' => 'half day'))}}
+                    </div>
+                    <div class="col-md-1">
+                        {{ Form::label('type','Type:', array('style' => 'font-size: 20px;')) }}
+                    </div>
+                    <div class="col-md-5">
+                        {{ Form::text('pay_type',null, array('class' => 'form-control', 'placeholder' => 'sick leave'))}}
+                    </div>
+                </div>
+                {!! Form::close() !!}
+                {!! Form::open(['url' => 'foo/bar']) !!}
+                <div class="row">
+                    <div class="col-md-1">
+                        {{ Form::label('from_date','From:', array('style' => 'font-size: 20px;')) }}
+                    </div>
+                    <div class="col-md-5">
+                        {{ Form::text('from_date',null, array('class' => 'form-control','placeholder' => 'yyyy/mm/dd'))}}
+                    </div>
+                    <div class="col-md-1">
+                        {{ Form::label('to_date','To:', array('style' => 'font-size: 20px;')) }}
+                    </div>
+                    <div class="col-md-5">
+                        {{ Form::text('to_date',null, array('class' => 'form-control','placeholder' => 'yyyy/mm/dd'))}}
+                    </div>
+                </div>
+                {!! Form::close() !!}
+                {!! Form::open(['url' => 'foo/bar']) !!}
+                <div class="row">
+                    <div class="col-md-4">
+                        {{ Form::label('hour_off','Total number of working days off:', array('style' => 'font-size: 20px;')) }}
+                    </div>
+                    <div class="col-md-8">
+                        {{ Form::text('contact_number',null, array('class' => 'form-control'))}}
+                    </div>
+                </div>
+                {!! Form::close() !!}
+                <hr>
+                <h2>Reason:</h2>
+                <br>
+                {!! Form::open(['url' => 'foo/bar']) !!}
+                {{ Form::textarea('reason',null, array('class' => 'form-control'))}}
+                {!! Form::close() !!}
+                <br>
+                {{ Form::submit('Request', array('class'=>'btn btn-success btn-block btn-lg')) }}
+                {!! Form::close() !!}
+            </fieldset>
+        </div>
+    </div>
 @endsection
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -14,8 +14,9 @@ class ComposerStaticInit19007f782ba0e1feba2a75a78644ea61
         '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         'f0906e6318348a765ffb6eb24e0d0938' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
         '58571171fd5812e6e447dce228f52f4d' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Support/helpers.php',
-        '801c31d8ed748cfa537fa45402288c95' => __DIR__ . '/..' . '/psy/psysh/src/functions.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+        '801c31d8ed748cfa537fa45402288c95' => __DIR__ . '/..' . '/psy/psysh/src/functions.php',
+        'f18cc91337d49233e5754e93f3ed9ec3' => __DIR__ . '/..' . '/laravelcollective/html/src/helpers.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -102,6 +103,7 @@ class ComposerStaticInit19007f782ba0e1feba2a75a78644ea61
         'C' => 
         array (
             'Cron\\' => 5,
+            'Collective\\Html\\' => 16,
             'Carbon\\' => 7,
         ),
         'A' => 
@@ -114,8 +116,8 @@ class ComposerStaticInit19007f782ba0e1feba2a75a78644ea61
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'XdgBaseDir\\' => 
         array (
@@ -269,6 +271,10 @@ class ComposerStaticInit19007f782ba0e1feba2a75a78644ea61
         array (
             0 => __DIR__ . '/..' . '/dragonmantank/cron-expression/src/Cron',
         ),
+        'Collective\\Html\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laravelcollective/html/src',
+        ),
         'Carbon\\' => 
         array (
             0 => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon',
@@ -343,6 +349,13 @@ class ComposerStaticInit19007f782ba0e1feba2a75a78644ea61
         'Carbon\\CarbonInterval' => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon/CarbonInterval.php',
         'Carbon\\Exceptions\\InvalidDateException' => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon/Exceptions/InvalidDateException.php',
         'Carbon\\Translator' => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon/Translator.php',
+        'Collective\\Html\\Componentable' => __DIR__ . '/..' . '/laravelcollective/html/src/Componentable.php',
+        'Collective\\Html\\Eloquent\\FormAccessible' => __DIR__ . '/..' . '/laravelcollective/html/src/Eloquent/FormAccessible.php',
+        'Collective\\Html\\FormBuilder' => __DIR__ . '/..' . '/laravelcollective/html/src/FormBuilder.php',
+        'Collective\\Html\\FormFacade' => __DIR__ . '/..' . '/laravelcollective/html/src/FormFacade.php',
+        'Collective\\Html\\HtmlBuilder' => __DIR__ . '/..' . '/laravelcollective/html/src/HtmlBuilder.php',
+        'Collective\\Html\\HtmlFacade' => __DIR__ . '/..' . '/laravelcollective/html/src/HtmlFacade.php',
+        'Collective\\Html\\HtmlServiceProvider' => __DIR__ . '/..' . '/laravelcollective/html/src/HtmlServiceProvider.php',
         'Cron\\AbstractField' => __DIR__ . '/..' . '/dragonmantank/cron-expression/src/Cron/AbstractField.php',
         'Cron\\CronExpression' => __DIR__ . '/..' . '/dragonmantank/cron-expression/src/Cron/CronExpression.php',
         'Cron\\DayOfMonthField' => __DIR__ . '/..' . '/dragonmantank/cron-expression/src/Cron/DayOfMonthField.php',
